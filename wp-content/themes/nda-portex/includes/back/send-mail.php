98dc1c781eace5bb4293c9443dc54e3f53675694
Dolmat <anton.dolmat@gmail.com>
2016-07-25 12:51:36 +0300

    test

--- wp-content/themes/nda-portex/includes/back/send-mail.php
+++ wp-content/themes/nda-portex/includes/back/send-mail.php
@@ -1,7 +1,7 @@
 <?php
 
 function send_mail() {
-
+  $type = 'asd';
   if ( $_POST['type'] === 'write-us-letter') {
     $type = 'Напишите нам';
   }
@@ -17,12 +17,12 @@ function send_mail() {
       $callback = 'Нет';
     }
     $letter = '<div><h1>Сообщение с сайта Portex-NDA из формы "' . $type . '"</h1>';
-    $letter += '<p><b>Имя клиента:</b> ' . $name . '<br>';
-    $letter += '<b>Электронная почта:</b> ' . $email . '<br>';
-    $letter += '<b>Сообщение:</b><br>' . $message . '<br>';
-    $letter += '<b>Телефон:</b> ' . $tel . '<br>';
-    $letter += '<b>Перезвонить:</b> ' . $callback;
-    $letter += '</p></div>';
+    // $letter += '<p><b>Имя клиента:</b> ' . $name . '<br>';
+    // $letter += '<b>Электронная почта:</b> ' . $email . '<br>';
+    // $letter += '<b>Сообщение:</b><br>' . $message . '<br>';
+    // $letter += '<b>Телефон:</b> ' . $tel . '<br>';
+    // $letter += '<b>Перезвонить:</b> ' . $callback;
+    // $letter += '</p></div>';
     wp_mail( 'portex.nda@gmail.com', 'Call-us-form', $letter);
     return true;
   }
