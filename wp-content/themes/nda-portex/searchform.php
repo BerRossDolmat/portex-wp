059448e078874ca42449530eff1e50450c594a3c
Dolmat <anton.dolmat@gmail.com>
2016-07-25 12:20:44 +0300

    test

--- wp-content/themes/nda-portex/searchform.php
+++ wp-content/themes/nda-portex/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>" class="search-form search-wrapper cf">
-  <input name="s" id="s" value="<?php echo wp_specialchars($s, 1); ?>" type="search" placeholder="Что Вы ищете?" required="">
+  <input name="s" id="s" value="" type="search" placeholder="Что Вы ищете?" required="">
   <button type="submit"><i class="search-btn material-icons">search</i></button>
 </form>
