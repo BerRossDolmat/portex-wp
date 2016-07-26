a935797d4be490843606587341d38e2620094c71
Dolmat <anton.dolmat@gmail.com>
2016-07-26 14:53:58 +0300

    contacts form

--- wp-content/themes/nda-portex/includes/back/send-mail.php
+++ wp-content/themes/nda-portex/includes/back/send-mail.php
@@ -1,12 +1,9 @@
 <?php
 
 function send_mail() {
-  $type = 'asd';
-  if ( $_POST['type'] === 'write-us-letter') {
-    $type = 'Напишите нам';
-  }
 
   if ($_POST['type'] === 'write-us-letter') {
+    $type = 'Напишите нам';
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
@@ -25,24 +22,32 @@ function send_mail() {
             . '<b>Перезвонить:</b> ' . $callback
             . '</p></div>';
 
-    wp_mail( 'portex.nda@gmail.com', 'Call-us-form', $letter);
+    wp_mail( 'portex.nda@gmail.com', 'Написать письмо', $letter);
     return true;
   }
 
   if ($_POST['type'] === 'contact-us') {
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $message = $_POST['message'];
-      $tel = $_POST['tel'];
-      if ( $_POST['callback'] === true) {
-        $callback = 'Да';
-      } else {
-        $callback = 'Нет';
-      }
-
-      wp_mail( 'portex.nda@gmail.com', $_POST['name'], $_POST);
-      return true;
+    $type = 'Форма контактов';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $tel = $_POST['tel'];
+    if ( $_POST['callback'] === 'true') {
+      $callback = 'Да';
+    } else {
+      $callback = 'Нет';
     }
+    $letter = '<div><h1>Сообщение с сайта Portex-NDA из формы "' . $type . '"</h1>'
+            . '<p><b>Имя клиента:</b> ' . $name . '<br>'
+            . '<b>Электронная почта:</b> ' . $email . '<br>'
+            . '<b>Сообщение:</b><br>' . $message . '<br>'
+            . '<b>Телефон:</b> ' . $tel . '<br>'
+            . '<b>Перезвонить:</b> ' . $callback
+            . '</p></div>';
 
-    return false;
+    wp_mail( 'portex.nda@gmail.com', 'Форма контактов', $letter);
+    return true;
+  }
+
+  return false;
 }
